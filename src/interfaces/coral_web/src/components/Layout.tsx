910433857fd5657c2b13133261c326fea92537ce
elisehammarstrom <32701757+elisehammarstrom@users.noreply.github.com>
2024-07-01 14:46:48 +0200

    changed style of searchbar

--- src/interfaces/coral_web/src/components/Layout.tsx
+++ src/interfaces/coral_web/src/components/Layout.tsx
@@ -72,104 +72,74 @@ export const Layout: React.FC<Props> = ({ title = 'Chat', children }) => {
     <>
     <PageHead title={capitalize(title)} />
 
-    <div className="fixed flex flex-col h-screen w-full bg-black">
+    <div className="fixed flex flex-col h-screen w-full">
 
-      <div className="h-2/5 w-full bg-mindorange flex flex-row justify-end">
-      {/* First section content */}
+      {/* <div className="h-2/5 w-full bg-mindorange flex flex-row justify-end"> */}
+      <div className="h-2/5 w-full flex flex-row justify-end">
+      {/* First horizontal section content */}
 
-      <div className="flex justify-center mb-4 w-full flex flex-col w-1/3 bg-white h-full">
-        {/* left section content */}
-      </div>
+        {/* <div className="flex justify-center mb-4 w-full flex flex-col w-1/4 bg-white h-full"> */}
+        <div className="flex justify-center mb-4 w-full flex flex-col w-1/4 h-full">
+          {/* left section content in first horizontal section */}
+        </div>
 
-      <div className="flex mb-4 w-full flex flex-col mt-auto w-1/3">
-            {/* middle section content */}
-          <div className="flex flex-col items-center">
-            <h1 className="text-xl font-bold">Sök här</h1>
-            <div className="flex flex-row items-center">
-              <SearchBar className="flex-grow" />
-              {/* <button className="ml-4 px-4 py-2 text-white bg-blue-500 rounded">Options</button> */}
+        <div className="flex mb-4 w-full h-full flex flex-col mt-auto w-2/4">
+              {/* middle section content in first horizontal section */}
+            <div className="flex flex-col items-center w-full h-full justify-end">
+              <h1 className="text-xl font-bold">Sök här</h1>
+              <h2 className="text-l font-bold">Subheadline</h2>
+              <div className="flex flex-col my-4 items-center w-full">
+                <SearchBar/>
+                <button className="w-full my-4 md:w-auto px-6 py-3 bg-mindorange border-mindorange text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all">Options</button>
 
-              <button className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all">Options</button>
-
-            </div>
-            
-          </div>
-      </div>
-
-      <div className="flex justify-center mb-4 w-full flex flex-col w-1/3 bg-black h-full">
-        {/* right section content */}
-      </div>
-    
-    
-    </div>
-
-    
-
-    <div className="h-3/5 w-full bg-mindorange">
-      {/* Second section content */}
-
-      <div className="h-1/2 w-full bg-mindmist border-t-5 border-gray-200">
-        {/* 2nd section content */}
-      </div>
-
-      <div className="h-1/2 w-full bg-mindblue">
-        {/* 3nd section content */}
-      </div>
-    </div>
-
-
-
-      
-
-
-    </div>
-    {/* // old: */}
-      {/* <PageHead title={capitalize(title)} />
-      <div className="flex flex-col h-screen w-full p-3 bg-white">
-        <header className="flex items-center justify-between p-4 border-b border-gray-200">
-          <h1 className="text-xl font-bold">Home Page</h1>
-          {userMenu}
-        </header>
-        <main className="flex flex-grow flex-col gap-4 p-4">
-          <div className="flex items-center justify-between">
-            <SearchBar className="flex-grow" />
-            <button className="ml-4 px-4 py-2 text-white bg-blue-500 rounded">Swe/Eng search Options</button>
-          </div>
-
-          <section className="flex flex-col gap-4">
-            <h2 className="text-lg font-semibold">Your selection for lookout:</h2>
-            <div className="flex gap-4">
-              <button className="px-4 py-2 text-white bg-gray-500 rounded">Theme 1: Suicide prevention</button>
-              <button className="px-4 py-2 text-white bg-gray-500 rounded">Theme 2: Medical treatments for x</button>
-              <select className="px-4 py-2 border border-gray-300 rounded">
-                <option>Dropdown of known themes to select from</option>
-                <option>Have default</option>
-              </select>
-            </div>
-            <button className="self-start px-4 py-2 text-white bg-green-500 rounded">Save</button>
-          </section>
-          <section className="flex gap-4">
-            <div className="flex-grow p-4 border border-gray-300 rounded">
-              <h3 className="mb-2 font-semibold">Recent Pubs</h3>
-              <div className="mb-2">
-                <h4 className="font-bold">Title</h4>
-                <p>Summary</p>
               </div>
-              <div>
-                <h4 className="font-bold">Title</h4>
-                <p>Summary</p>
-              </div>
+              
             </div>
-            <div className="flex-grow p-4 border border-gray-300 rounded">
-              <h3 className="mb-2 font-semibold">Updates for your saved themes</h3>
-              {/* Add content here */}
-            {/* </div>
-          </section>
+        </div>
 
-        </main>
-        
+      {/* <div className="flex justify-center mb-4 w-full flex flex-col w-1/4 bg-black h-full"> */}
+      <div className="flex justify-center mb-4 w-full flex flex-col w-1/4 h-full">
+        {/* right section content in first horizontal section */}
+      </div>
+    
+    
+    </div>
 
-      </div> */}
+    
+
+    {/* <div className="h-3/5 w-full bg-mindorange"> */}
+    <div className="h-3/5 w-full">
+      {/* Second horizontal section content */}
+
+      {/* <div className="h-1/2 w-full bg-mindmist border-t-5 border-gray-200"> */}
+      <div className="h-1/2 w-full border-t-5 border-gray-200">
+        {/* 1st section content in 2nd horizontal section */}
+      </div>
+
+      {/* <div className="h-1/2 w-full bg-mindblue"> */}
+      <div className="h-1/2 w-full">
+          {/* 2nd section content in 2nd horizontal section */}
+      </div>
+    </div>
+
+    {/* <svg viewBox="0 0 1024 1024"
+              class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+              aria-hidden="true">
+              <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7">
+              </circle>
+              <defs>
+                  <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+                      <stop stop-color="#F17509"></stop>
+                      <stop offset="1" stop-color="#FFE1D4"></stop>
+                  </radialGradient>
+              </defs>
+          </svg> */}
+
+
+    </div>
+
+    
+
     </>
   );
 };
--- src/interfaces/coral_web/src/components/Shared/SearchBar/SearchBar.tsx
+++ src/interfaces/coral_web/src/components/Shared/SearchBar/SearchBar.tsx
@@ -18,57 +18,26 @@ export const SearchBar: React.FC<Props> = ({ className }) => {
   };
 
   return (
-    // old search bar
-    // <div className={`flex items-center ${className}`}>
-    //   <input
-    //     type="text"
-    //     value={query}
-    //     onChange={handleInputChange}
-    //     placeholder="Search..."
-    //     className="flex-grow px-4 py-2 border border-gray-300 rounded-l"
-    //   />
-    //   <button
-    //     onClick={handleSearch}
-    //     className="px-4 py-2 text-white bg-blue-500 rounded-r"
-    //   >
-    //     Search
-    //   </button>
-    // </div>
-
-  <div className={`flex items-center ${className}`}>
-
-      <form action="/search" className="mx-auto mt-8 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300">
-        <input 
-          type="text" 
-          placeholder="Sök här..." 
-          className="flex-grow p-2 border rounded-md"
-        />
-        <button 
-          type="submit" 
-          className="w-full md:w-auto px-8 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all"
-        >
-          <div className="flex items-center transition-all opacity-1">
-            <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
-              Search
-            </span>
-          </div>
-        </button>
-      </form> 
-
-    <svg viewBox="0 0 1024 1024"
-            class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
-            aria-hidden="true">
-            <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7">
-            </circle>
-            <defs>
-                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
-                    <stop stop-color="#3b82f6"></stop>
-                    <stop offset="1" stop-color="#1d4ed8"></stop>
-                </radialGradient>
-            </defs>
-        </svg>
-  </div>
-    
+    <div className={"flex items-center w-3/4"}>
+        <form action="/search" className="mx-auto mt-8 bg-white relative w-full flex flex-col md:flex-row items-center justify-center py-2 px-2 rounded-2xl gap-4 box-shadow shadow-xl shadow-mindapricot focus-within:border-gray-300 border-opacity-50 border border-mindapricot">
+          <input 
+            type="text" 
+            placeholder="Sök här..." 
+            className="flex-grow p-4 rounded-2xl text-xl border-opacity-30 border border-gray-300"
+          />
+          <button 
+            type="submit" 
+            className="w-full md:w-auto px-8 py-3 bg-mindorange border-mindorange text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all"
+          >
+            <div className="flex items-center transition-all opacity-1">
+              <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+                Sök
+              </span>
+            </div>
+          </button>
+        </form> 
+    </div>
+      
   );
 };
 
